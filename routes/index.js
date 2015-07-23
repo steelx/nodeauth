9026e212ccace909d1afa2a5e75e27740febccab
Steel <ajinkya@riva.co>
2015-07-23 16:08:31 +0530

    Restricted access to Members page index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', ensureAuthenticated,function(req, res, next) {
   res.render('index', { title: 'Members' });
 });
+
+
+//restrict members page
+function ensureAuthenticated(req, res, next) {
+  if(req.isAuthenticated()){
+    //logged in
+    return next();
+  }
+
+  //Not logged in
+  res.redirect('/users/login');
+}
 
 module.exports = router;
